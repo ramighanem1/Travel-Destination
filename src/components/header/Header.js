23f80ca376155699f4c12abbec1644646b236766
ramighanem1 <rami.g.12@hotmail.com>
2023-03-08 12:24:48 +0300

    add my work

---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+
+import './Header.css';
+
+function Header() {
+    return (
+        <>
+            <header className="header">
+                {/* <img src="./Data/Logo.jpg" alt="Logo"> */}
+                <h1>Hello From Header</h1>
+                {/* <nav>
+                        <a href="./">Home</a>
+                        <a href="./accounting.html">Accounting</a>
+                    </nav> */}
+            </header>
+        </>
+    );
+}
+
+export default Header;
